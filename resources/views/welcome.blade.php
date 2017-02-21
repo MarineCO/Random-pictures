a1cbf87a727bb23c642cd2bd43ed2642bce3e413
marineco <marinecolonge@gmail.com>
2017-02-21 22:57:02 +0100

    add css with semantic

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,36 @@
 </head>
 <body>
 
-<h1>Hello Laure</h1>
+    <div class="ui centered grid">
+        <div class="column">
+            <div class="ui center aligned container">
+                <h1>Random Pictures</h1>
+                <h2>"Take your beautiful picture !"</h2>
+                <img src="http://placehold.it/900x400"/>
+                <div class="massive ui fluid teal button">Change the image</div>
+                <div class="ui big fluid action input">
+                    <input type="text" value="http://www.heroku.it/900x400"/>
+                    <button class="ui massive yellow right labeled icon button">
+                        <i class="copy icon"></i>
+                        Copy
+                    </button>
+                </div>
+                <div class="ui massive form">
+                    <div class="two fields">
+                        <div class="field">
+                            <label for="width">Width</label>
+                            <input id="width" type="text" name="width" value="900">
+                        </div>
+                        <div class="field">
+                            <label for="height">Height</label>
+                            <input id="height" type="text" name="height" value="400">
+                        </div>
+                    </div>
+                    <div class="massive ui submit button">Resize</div>
+                </div>
+            </div>
+        </div>
+    </div>
 
     <script> window.Laravel = { csrfToken : "{{csrf_token()}}"}; </script>
     <script src="{{mix('/js/app.js')}}"></script>
