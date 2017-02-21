6a8c39b5cfdab01ce9cc952759edbde15f8f6e73
marineco <marinecolonge@gmail.com>
2017-02-21 16:32:56 +0100

    initial commit

---
+++ resources/assets/js/bootstrap.js
@@ -0,0 +1,10 @@
+window.$ = window.jQuery = require('jquery');
+
+require('bootstrap-sass');
+
+window.axios = require('axios');
+
+window.axios.defaults.headers.common = {
+    'X-CSRF-TOKEN': window.Laravel.csrfToken,
+    'X-Requested-With': 'XMLHttpRequest'
+};
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8"/>
+    <title>Random Pictures</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/semantic-ui/2.2.7/semantic.min.css">
+    <link rel="stylesheet" href="{{mix('/css/app.css')}}">
+</head>
+<body>
+
+<h1>Hello Laure</h1>
+
+    <script> window.Laravel = { csrfToken : "{{csrf_token()}}"}; </script>
+    <script src="{{mix('/js/app.js')}}"></script>
+</body>
+</html>
