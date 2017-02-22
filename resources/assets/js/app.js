6f936396004a036a39cdfca05800bfbcf5a09390
marineco <marinecolonge@gmail.com>
2017-02-22 11:55:54 +0100

    random picture when we click on button : choose another picture

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,3 +1,25 @@
 require('./bootstrap');
 
-console.log('hello');
+(function() {
+
+	'use strict';
+
+	let app = {
+
+		init: function() {
+			this.listeners();
+		},
+
+		listeners: function() {
+			$('#choose').on('click', this.changePicture);
+		},
+
+		changePicture: function() {
+			let randomNum = Math.floor((Math.random()*10)+1);
+			$('#pics').attr('src', 'img/img-' + randomNum + '.jpg');
+		}
+	}
+
+	app.init();
+
+})();
