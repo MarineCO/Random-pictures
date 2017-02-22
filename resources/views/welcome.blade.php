4056dd1691917ce8e950f688d65420980d547949
marineco <marinecolonge@gmail.com>
2017-02-22 09:18:25 +0100

    change image 4 and 6 + add view content

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,15 +13,8 @@
             <div class="ui center aligned container">
                 <h1>Random Pictures</h1>
                 <h2>"Take your beautiful picture !"</h2>
-                <img src="http://placehold.it/900x400"/>
-                <div class="massive ui fluid teal button">Change the image</div>
-                <div class="ui big fluid action input">
-                    <input type="text" value="http://www.heroku.it/900x400"/>
-                    <button class="ui massive yellow right labeled icon button">
-                        <i class="copy icon"></i>
-                        Copy
-                    </button>
-                </div>
+                <img src="/../img/img-4.jpg"/>
+                <div class="massive ui fluid teal button">Choose another picture</div>
                 <div class="ui massive form">
                     <div class="two fields">
                         <div class="field">
@@ -35,11 +28,20 @@
                     </div>
                     <div class="massive ui submit button">Resize</div>
                 </div>
+                <br>
+                <div class="ui big fluid action input">
+                    <input type="text" value="http://www.heroku.it/900x400"/>
+                    <button class="ui massive yellow right labeled icon button">
+                        <i class="copy icon"></i>
+                        Copy
+                    </button>
+                </div>
             </div>
         </div>
     </div>
 
     <script> window.Laravel = { csrfToken : "{{csrf_token()}}"}; </script>
     <script src="{{mix('/js/app.js')}}"></script>
+    <script src="https://cdn.jsdelivr.net/clipboard.js/1.6.0/clipboard.min.js"></script>
 </body>
 </html>
